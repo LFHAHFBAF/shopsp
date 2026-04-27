bb25ea3f3068d3313018ea7656a76c0589e5470a
LFHAHFBAF <105861901+LFHAHFBAF@users.noreply.github.com>
2026-04-27 21:52:54 +0800

    Update and rename ner noJeffrey.lua to new noJeffrey.lua

--- ner noJeffrey.lua
+++
@@ -1,7 +0,0 @@
-while true do 
-    local jeffrey = workspace:FindFirstChild("True Jeffrey")
-    if jeffrey then
-        jeffrey:Destroy()
-    end
-    task.wait(10) 
-end
---
+++ new noJeffrey.lua
@@ -0,0 +1,7 @@
+while true do 
+    local newjeffrey = workspace:FindFirstChild("True Jeffrey")
+    if jeffrey then
+        newjeffrey:Destroy()
+    end
+    task.wait(10) 
+end
